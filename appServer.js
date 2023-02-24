3481b8ae70fdb7c02b32e958cce79690cece13b6
s4r1m <sarimsohail81@gmail.com>
2023-02-24 13:30:03 -0500

    commit

--- appServer.js
+++ appServer.js
@@ -1,73 +1,4 @@
-
-
-// async function main() {
-
-//     try {
-//         let args = process.argv.slice(2);
-
-//         const identityLabel = args[0];
-//         const functionName = args[1];
-//         const chaincodeArgs = args.slice(2);
-
-//         const orgName = identityLabel.split('@')[1];
-//         const orgNameWithoutDomain = orgName.split('.')[0];
-
-//         let connectionProfile = JSON.parse(fs.readFileSync(
-//             path.join(testNetworkRoot,
-//                 'organizations/peerOrganizations',
-//                 orgName,
-//                 `/connection-${orgNameWithoutDomain}.json`), 'utf8')
-//         );
-
-//         let connectionOptions = {
-//             identity: identityLabel,
-//             wallet: wallet,
-//             discovery: { enabled: true, asLocalhost: true }
-//         };
-
-//         console.log('Connect to a Hyperledger Fabric gateway.');
-//         const check = await gateway.connect(connectionProfile, connectionOptions);
-
-//         console.log("Wallet: ", JSON.stringify(wallet));
-//         console.log("Gateway: ", JSON.stringify(check));
-
-//         console.log('Use channel "mychannel".');
-//         const network = await gateway.getNetwork('mychannel');
-
-//         console.log('Use SupplyChain.');
-//         const contract = network.getContract('supply_chain');
-
-//         console.log('Submit ' + functionName + ' transaction.');
-//         const response = await contract.submitTransaction(functionName, ...chaincodeArgs);
-//         if (`${response}` !== '') {
-//             console.log(`Response from ${functionName}: ${response}`);
-//         }
-
-//     } catch (error) {
-//         console.log(`Error processing transaction. ${error}`);
-//         console.log(error.stack);
-//     } finally {
-//         console.log('Disconnect from the gateway.');
-//         gateway.disconnect();
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 'use strict';
-
-
 var express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -90,7 +21,7 @@ app.get('/api/getList', async function (req, res) {
     try {
 
         const gateway = new Gateway();
-        const wallet = await Wallets.newFileSystemWallet('./wallet');
+        const wallet = await Wallets.newFileSystemWallet('./supply_chain_app/wallet');
 
         const identityLabel = req.body.label;
         const functionName = req.body.function;
