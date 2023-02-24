7874d832f2d39bc588dabf1dc9bf2e8d7daab9e9
s4r1m <sarimsohail81@gmail.com>
2023-02-24 13:17:15 -0500

    commit

--- appServer.js
+++ appServer.js
@@ -1,23 +1,105 @@
+
+
+async function main() {
+
+    try {
+        let args = process.argv.slice(2);
+
+        const identityLabel = args[0];
+        const functionName = args[1];
+        const chaincodeArgs = args.slice(2);
+
+        const orgName = identityLabel.split('@')[1];
+        const orgNameWithoutDomain = orgName.split('.')[0];
+
+        let connectionProfile = JSON.parse(fs.readFileSync(
+            path.join(testNetworkRoot,
+                'organizations/peerOrganizations',
+                orgName,
+                `/connection-${orgNameWithoutDomain}.json`), 'utf8')
+        );
+
+        let connectionOptions = {
+            identity: identityLabel,
+            wallet: wallet,
+            discovery: { enabled: true, asLocalhost: true }
+        };
+
+        console.log('Connect to a Hyperledger Fabric gateway.');
+        const check = await gateway.connect(connectionProfile, connectionOptions);
+
+        console.log("Wallet: ", JSON.stringify(wallet));
+        console.log("Gateway: ", JSON.stringify(check));
+
+        console.log('Use channel "mychannel".');
+        const network = await gateway.getNetwork('mychannel');
+
+        console.log('Use SupplyChain.');
+        const contract = network.getContract('supply_chain');
+
+        console.log('Submit ' + functionName + ' transaction.');
+        const response = await contract.submitTransaction(functionName, ...chaincodeArgs);
+        if (`${response}` !== '') {
+            console.log(`Response from ${functionName}: ${response}`);
+        }
+
+    } catch (error) {
+        console.log(`Error processing transaction. ${error}`);
+        console.log(error.stack);
+    } finally {
+        console.log('Disconnect from the gateway.');
+        gateway.disconnect();
+    }
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'use strict';
+
+
 var express = require('express');
-var bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
+const { Wallets, Gateway } = require('fabric-network');
+const testNetworkRoot = path.resolve(require('os').homedir(), 'fabric-samples/test-network');
 
 const port = 3000
 var app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Setting for Hyperledger Fabric
-const { Wallets, Gateway } = require('fabric-network');
-const path = require('path');
-const ccpPath = path.resolve(__dirname, 'connections', `connection-org1.json`);
+// const ccpPath = path.resolve(__dirname, 'connections', `connection-org1.json`);
 // const ccpPath = path.resolve(__dirname, '.', 'connection-org1.json');
+const gateway = new Gateway();
+const wallet = await Wallets.newFileSystemWallet('./wallet');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/api/queryallcars', async function (req, res) {
+app.get('/api/getList', async function (req, res) {
     try {
+
+        console.log(req.body)
+        // const identityLabel = req.body.label;
+        // const functionName = req.body.;
+        // const chaincodeArgs = args.slice(2);
+
+
 
         console.log("In API")
 
