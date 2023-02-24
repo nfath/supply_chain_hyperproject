24e7e93ffa84bdd94218b21c3ccea80e800e0495
s4r1m <sarimsohail81@gmail.com>
2023-02-24 14:25:46 -0500

    commit

--- appServer.js
+++ appServer.js
@@ -49,7 +49,7 @@ app.get('/api/getList', async function (req, res) {
 
         console.log('Submit ' + functionName + ' transaction.');
 
-        const chainCodearguments = JSON.stringify(chaincodeArgs)
+        const chainCodearguments = (chaincodeArgs)
         console.log("Arguemtns: ", ...chainCodearguments)
 
 
